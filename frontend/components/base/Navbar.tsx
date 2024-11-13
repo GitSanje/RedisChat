c7972011d4b21b43e982fcc26f21c31f2b30e0d3
Sanjay Karki <karkisanjay2002@gmail.com>
2024-11-13 21:38:07 +0545

    added prisma model

--- frontend/components/base/Navbar.tsx
+++ frontend/components/base/Navbar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import Link from "next/link";
 import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 import { Button } from "../ui/button";
-import LoginModal from "../auth/LoginModal";
+import LoginModal from "../auth/LoginModel";
 export default function Navbar({ user }: { user: CustomUser | null }) {
   return (
     <nav className="p-6 flex justify-between items-center bg-white shadow-sm">
-      <h1 className="text-xl md:text-2xl font-extrabold">QuickChat</h1>
+      <h1 className="text-xl md:text-2xl font-extrabold">RedisChat</h1>
       <div className="flex items-center space-x-2 md:space-x-6 text-gray-700">
         <Link href="/">Home</Link>
         <Link href="#features">Features</Link>
